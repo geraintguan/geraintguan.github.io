b313ee37c20611b39fbba81e680fc7bacefedec2
Geraint Guan <geraint.guan@icloud.com>
2023-05-29 19:30:02 +0000

    Landing Page WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,28 @@
-<div class="card w-96 bg-base-100 shadow-xl">
-	<div class="card-body">
-		<h2 class="card-title">Card title!</h2>
-		<p>If a dog chews shoes whose shoes does he choose?</p>
-		<div class="card-actions justify-end">
-			<button class="btn btn-primary">Buy Now</button>
+<div class="flex">
+	<div class="flex flex-col md:flex-row mx-auto md:mt-32">
+		<div class="flex flex-col p-12 md:p-24">
+			<div class="font-bold text-3xl">Geraint Guan</div>
+			<div class="text-xl">Lead Software Engineer at Appointedd</div>
 		</div>
+		<article class="prose lg:prose-xl px-12 md:px-4">
+			<p>
+				Ever since I stumbled across programming and made my first command-line tic-tac-toe game
+				written in C++ when I was 12, I've been enamoured with the art of writing code. After a
+				stint in academia studying Computer Science at the <a href="https://www.st-andrews.ac.uk/"
+					>University of Saint Andrews</a
+				>
+				I joined <a href="https://appointedd.com">Appointedd</a> and have had the great pleasure of working
+				on a great product with an amazing team of people.
+			</p>
+			<p>
+				I currently split my time between engineering management, product management, operational
+				infrastructure, and programming. I love being able to have exposure to and work on a large
+				number of areas as it provides ample opportunities to learn and grow.
+			</p>
+			<p>
+				In my downtime messing around with electronics, making music, and playing games (both
+				tabletop and digital).
+			</p>
+		</article>
 	</div>
 </div>
