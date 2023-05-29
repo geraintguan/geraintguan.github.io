166fcb113eaac362af28c9c9933ce62c5930a3c8
Geraint Guan <geraint.guan@icloud.com>
2023-05-29 19:30:02 +0000

    Add tailwind css & daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="card w-96 bg-base-100 shadow-xl">
+	<div class="card-body">
+		<h2 class="card-title">Card title!</h2>
+		<p>If a dog chews shoes whose shoes does he choose?</p>
+		<div class="card-actions justify-end">
+			<button class="btn btn-primary">Buy Now</button>
+		</div>
+	</div>
+</div>
