510d340602d26ee9c3b4c8c591f2f7416bd3e591
Geraint Guan <geraint.guan@icloud.com>
2024-03-13 23:08:16 +0000

    Fix components folder not being included in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./renderer/**/*.{js,ts,jsx,tsx,mdx}",
   ],
