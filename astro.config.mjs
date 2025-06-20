fcc3ef00c305ce99a440dea833b5012bb97ce2f3
Geraint Guan <geraint.guan@icloud.com>
2025-06-20 10:16:19 +0000

    Add astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 // @ts-check
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [icon()],
+
   site: "https://geraint.io",
+
   vite: {
     plugins: [tailwindcss()],
   },
