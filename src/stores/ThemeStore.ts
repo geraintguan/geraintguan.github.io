165d5fb1c2b90161c768dcce892ffd5626dcd1a0
Geraint Guan <geraint.guan@icloud.com>
2025-06-29 20:44:20 +0000

    Default to system theme

--- src/stores/ThemeStore.ts
+++ src/stores/ThemeStore.ts
@@ -1,6 +1,6 @@
 import { persistentAtom } from "@nanostores/persistent";
 
-export type Theme = "dark" | "light";
+export type Theme = "dark" | "light" | undefined;
 
 export const theme = persistentAtom<Theme>("theme", "light", {
   decode: (value) => {
@@ -13,5 +13,26 @@ export const theme = persistentAtom<Theme>("theme", "light", {
 
     return value;
   },
-  encode: (value) => value,
+  encode: (value) => value ?? "light",
 });
+
+export function init() {
+  theme.subscribe((value) => {
+    if (!value) {
+      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+        .matches
+        ? "dark"
+        : "light";
+
+      console.log(`No current theme set, using system theme '${systemTheme}'`);
+
+      document.documentElement.setAttribute("data-theme", systemTheme);
+
+      theme.set(systemTheme);
+    } else {
+      console.log(`Setting theme to '${value}'`);
+
+      document.documentElement.setAttribute("data-theme", value);
+    }
+  });
+}
