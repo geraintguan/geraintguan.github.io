290d0eb914bb490cf0830f5b8d57596be554b8ed
Geraint Guan <geraint.guan@icloud.com>
2025-07-04 15:52:13 +0000

    Refactor ThemeStore as class

--- src/stores/ThemeStore.ts
+++ src/stores/ThemeStore.ts
@@ -1,38 +1,80 @@
+import type { WritableAtom } from "nanostores";
+
 import { persistentAtom } from "@nanostores/persistent";
 
-export type Theme = "dark" | "light" | undefined;
+export type Theme = "dark" | "light" | "unset";
 
-export const theme = persistentAtom<Theme>("theme", "light", {
-  decode: (value) => {
-    if (value !== "dark" && value !== "light") {
-      console.warn(
-        `Expected value of persistent atom "theme" to be "dark" or "light", but got "${value}", Defaulting to "light"`,
-      );
-      return "light";
+export class ThemeStore {
+  public static Instance = new ThemeStore();
+
+  private readonly atom: WritableAtom<Theme>;
+
+  constructor() {
+    this.atom = persistentAtom<Theme>("theme", "unset", {
+      decode: (value) => {
+        if (value !== "dark" && value !== "light") {
+          console.warn(
+            `Expected value of persistent atom 'theme' to be one of ['dark','light','unset'], but got '${value}', Defaulting to 'unset'`,
+          );
+
+          return "unset";
+        }
+
+        return value;
+      },
+      encode: (value) => value,
+    });
+  }
+
+  registerSubscription(): void {
+    this.atom.subscribe((value) => {
+      if (value === "unset") {
+        const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+          .matches
+          ? "dark"
+          : "light";
+
+        console.log(
+          `No current theme set, using system theme '${systemTheme}'`,
+        );
+
+        document.documentElement.setAttribute("data-theme", systemTheme);
+
+        this.atom.set(systemTheme);
+      } else {
+        console.log(`Setting theme to '${value}'`);
+
+        document.documentElement.setAttribute("data-theme", value);
+      }
+    });
+  }
+
+  registerToggleButtonById(id: string): void {
+    const button = document.getElementById(id);
+
+    if (!button) {
+      throw new Error(`Could not find element button#${id}`);
     }
 
-    return value;
-  },
-  encode: (value) => value ?? "light",
-});
+    button.addEventListener("click", () => {
+      const currentTheme = this.atom.get();
 
-export function init() {
-  theme.subscribe((value) => {
-    if (!value) {
-      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
-        .matches
-        ? "dark"
-        : "light";
+      if (currentTheme === "dark") {
+        this.atom.set("light");
+      } else {
+        this.atom.set("dark");
+      }
+    });
+  }
 
-      console.log(`No current theme set, using system theme '${systemTheme}'`);
+  toggle(): string {
+    const currentTheme = this.atom.get();
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
 
-      document.documentElement.setAttribute("data-theme", systemTheme);
+    this.atom.set(newTheme);
 
-      theme.set(systemTheme);
-    } else {
-      console.log(`Setting theme to '${value}'`);
-
-      document.documentElement.setAttribute("data-theme", value);
-    }
-  });
+    return newTheme;
+  }
 }
+
+export default ThemeStore.Instance;
